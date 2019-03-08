60c639f8990dd7617d4c9a5ba4beb1f2f43228c2
Ferris Boran <ferris.boran@gmail.com>
2019-03-08 20:46:50 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,19 @@
 def prime?(int)
-  check = [2,3,5,7,9,11,13,17,19,23]
+  check = [2,3]
+  x = 5
+  while check.size < 20
+  	check.each do |i|
+  		if x % i == 0
+  			x += 1
+  		end
+  	end
+  	check << x
+  	x += 1
+  end
+  
   if check.include?(int)
     return true
-  elsif int <= 1 || int % 2 == 0 || int % 3 == 0
+  elsif int <= 1
     return false
   elsif
     for i in check
@@ -12,3 +23,5 @@ def prime?(int)
     return true
   end
 end
+
+# x=4 and if x % any current element in check = 0 then x++ otherwise push to check. keep doing that until check's length is 20ish
