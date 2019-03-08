995ba53ae5f52ebc081f8894e34d9518cc4bbb61
Ferris Boran <ferris.boran@gmail.com>
2019-03-08 20:09:53 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,7 @@
 def prime?(int)
-  if int <= 1 || int % 2 == 0 || int % 3 == 0 && int != 2 && int != 3
+  if int == 2 || int == 3
+    return true
+  elsif int <= 1 || int % 2 == 0 || int % 3 == 0
     return false
   else
     return true
