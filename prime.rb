fa4f46e488176577c14abb24d5c69da8fcaff7fd
Ferris Boran <ferris.boran@gmail.com>
2019-03-08 21:08:45 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,26 +1,11 @@
 def prime?(int)
 
-  if int < 2 
+  if int < 2
     return false
   end
-
   (2..int - 1).each do |i|
     if int % i == 0
       return false
   end
-  return true
+  true
 end
-
-=begin
-primes = [2,3]
-x = 5
-while primes.size < 20
-  primes.each do |i|
-    if x % i == 0
-      x += 1
-    end
-  end
-  primes << x
-  x += 1
-end
-=end
