26882edae285d08c724d3d188f8a0e74895cdf04
Ferris Boran <ferris.boran@gmail.com>
2019-03-08 20:14:14 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,6 +3,10 @@ def prime?(int)
     return true
   elsif int <= 1 || int % 2 == 0 || int % 3 == 0
     return false
+  elsif
+    check = [2,3,5,7,9,11,13]
+    for i in check
+      int % i == 0 ? false : break
   else
     return true
   end
