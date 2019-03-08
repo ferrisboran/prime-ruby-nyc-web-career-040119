c5aa2c72b238cbabe86c23bc3edf53836e1906dc
Ferris Boran <ferris.boran@gmail.com>
2019-03-08 20:15:48 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
 def prime?(int)
-  if int == 2 || int == 3
+  check = [2,3,5,7,9,11,13]
+  if check.include?(int)
     return true
   elsif int <= 1 || int % 2 == 0 || int % 3 == 0
     return false
   elsif
-    check = [2,3,5,7,9,11,13]
     for i in check
       int % i == 0 ? false : break
     end
